8859776f0070368beb0a394e3fb9033172f5e37e
Samay Gandhi <gandhisamay200@gmail.com>
2022-09-08 00:38:02 +0300

    New amazing cp template :)

--- lua/user/snippets/cpp.lua
+++ lua/user/snippets/cpp.lua
@@ -25,6 +25,61 @@ using ll = long long;
 using vi = vector<int>;
 using ld = long double;
 using pii = pair<int, int>;
+using vvi = vector<vector<int>>;
+
+struct custom_hash
+{{
+  static uint64_t splitmix64(uint64_t x)
+  {{
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }}
+
+  size_t operator()(uint64_t x) const
+  {{
+    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }}
+}};
+
+template <typename T1, typename T2> // Key should be integer type
+using safe_map = unordered_map<T1, T2, custom_hash>;
+ 
+// Operator overloads
+template <typename T1, typename T2> // cin >> pair<T1, T2>
+istream &operator>>(istream &istream, pair<T1, T2> &p)
+{{
+  return (istream >> p.first >> p.second);
+}}
+ 
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v)
+{{
+  for (auto &it : v)
+    cin >> it;
+  return istream;
+}}
+ 
+template <typename T1, typename T2> // cout << pair<T1, T2>
+ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
+{{
+  return (ostream << p.first << " " << p.second);
+}}
+
+template <typename T> // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{{
+  for (auto &it : c)
+    cout << it << " ";
+  return ostream;
+}}
+ 
+// Utility functions
+ 
+template <typename T>
+int32_t size_i(T &container) {{ return static_cast<int32_t>(container.size()); }}
 
 void solve(){{
  {}
@@ -104,27 +159,66 @@ int findSet(int i, vector<int> &parent) {{
   return parent[i] = findSet(parent[i], parent);
 }}
 
-void unionSet(int i, int k, vector<int>& parent, vector<int>& rank){{
+void unionSet(int i, int k, vector<int> &parent, vector<int> &rank) {{
   int s1 = findSet(i, parent);
   int s2 = findSet(k, parent);
 
-  if(s1 != s2){{
-    if(rank[i] < rank[k]){{
-      parent[i] = k;
-      rank[k] += rank[i];
-    }}
-    else{{
-      parent[k] = i;
-      rank[i] += rank[k];
+  if (s1 != s2) {{
+    if (rank[s1] <= rank[s2])
+      swap(s1, s2);
+    rank[s1] += rank[s2];
+    parent[s2] = s1;
+  }}
+}}
+
+]], {}))
+
+local bfs = s('bfs', fmt([[ 
+void bfs(int src, vi &vis, vi adj[]) {{
+  vis[src] = 1;
+  queue<int> q;
+  q.push(src);
+
+  while (!q.empty()) {{
+    int temp = q.front();
+    q.pop();
+
+    for (auto nbr : adj[temp]) {{
+      if (!vis[nbr]) {{
+        q.push(nbr);
+        vis[nbr] = 1;
+      }}
     }}
   }}
 }}
 
 ]], {}))
 
+local bs = s('bs', fmt([[ 
+int binarySearch(int tar, vi nums) {{
+  int lo = -1;
+  int hi = nums.size();
+
+  while (lo + 1 < hi) {{
+    int mid = (hi - lo) / 2 + lo;
+    if (nums[mid] == tar)
+      return mid;
+    else if (nums[mid] > tar)
+      hi = mid;
+    else
+      lo = mid;
+  }}
+
+  return -1;
+}}
+
+]], {}))
+
 table.insert(snippets, cppBoilerplate)
-table.insert(snippets, spf);
+table.insert(snippets, spf)
 table.insert(snippets, binexp)
 table.insert(snippets, dsu)
+table.insert(snippets, bfs)
+table.insert(snippets, bs)
 
 return snippets, autosnippets
