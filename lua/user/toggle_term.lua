753378ae42d88d1cd06944f2c91ec9d97a4cc4ee
Samay Gandhi <gandhisamay200@gmail.com>
2022-11-17 20:11:30 +0530

    line wrap set to true

--- lua/user/toggle_term.lua
+++ lua/user/toggle_term.lua
@@ -6,9 +6,8 @@ local newterm = Terminal:new({
   dir = "%:p:h",
   direction = "horizontal",
   -- function to run on opening the terminal
-  on_open = function(term)
+  on_open = function(_)
     vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<leader>q", "<cmd>close<CR>", { noremap = true, silent = true })
   end,
   -- function to run on closing the terminal
   on_close = function(_)
